09f87a58b944933d7fc6f40e62ea7a057176cd3d
data7691 <data7691@gmail.com>
2026-06-17 17:05:50 +0800

    Add files via upload

---
+++ T_BigHotbling.java
@@ -0,0 +1,58 @@
+package com.add.BigHot;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.lineage.server.model.TimeInform;
+import com.lineage.server.thread.GeneralThreadPool;
+
+public class T_BigHotbling extends TimerTask {
+	private static T_BigHotbling _instance;
+	private Timer _timeHandler = new Timer(true);
+
+	private boolean _isBigHotta = false;
+
+	public static T_BigHotbling getStart() {
+		if (_instance == null) {
+			_instance = new T_BigHotbling();
+		}
+		return _instance;
+	}
+
+	private T_BigHotbling() {
+		this._timeHandler.schedule(this, 5000L, 10000L);
+
+		GeneralThreadPool.get().execute(this);
+	}
+
+	public void run() {
+		String mTime = TimeInform.time().getNow_YMDHMS(3);
+		String hTime = TimeInform.time().getNow_YMDHMS(4);
+		int mm = Integer.parseInt(mTime);
+		int hh = Integer.parseInt(hTime);
+		switch (hh) {
+		case 2:
+		case 6:
+		case 10:
+		case 14:
+		case 18:
+		case 22:
+			if ((mm == 1) && (!this._isBigHotta)) {
+				BigHotblingTime BigHot = new BigHotblingTime();
+				BigHot.startBigHotbling();
+				this._isBigHotta = true;
+			}
+
+			if ((mm == 3) && (this._isBigHotta)) {
+				this._isBigHotta = false;
+			}
+
+			break;
+		}
+
+	}
+
+	public void startT_BigHotbling() {
+		getStart();
+	}
+}
